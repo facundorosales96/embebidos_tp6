7650189b36ea63d9eea80a2ed0045805fb6258e6
Facundo Rosales <facundoerosales@gmail.com>
2023-06-06 19:07:02 -0300

    se agrega funciones del controlador de pantalla

--- inc/display.h
+++ inc/display.h
@@ -31,6 +31,7 @@ SPDX-License-Identifier: MIT
  ** @{ */
 
 /* === Headers files inclusions ================================================================ */
+#include <stdint.h>
 
 /* === Cabecera C++ ============================================================================ */
 
@@ -40,9 +41,63 @@ extern "C" {
 
 /* === Public macros definitions =============================================================== */
 
+// Definiciones de bits asociados a cada segmento para construir los digitos
+#define SEGMENT_A (1 << 0)
+#define SEGMENT_B (1 << 1)
+#define SEGMENT_C (1 << 2)
+#define SEGMENT_D (1 << 3)
+#define SEGMENT_E (1 << 4)
+#define SEGMENT_F (1 << 5)
+#define SEGMENT_G (1 << 6)
+#define SEGMENT_P (1 << 7)
+
 /* === Public data type declarations =========================================================== */
 
+//! Referencia a un descriptor para gestionar pantalla de siete segmentos
+typedef struct display_s * display_t;
+
+//! Funcion de callback para apagar los segmentos y digitos de la pantalla
+typedef void (*display_screen_off_t)(void);
+
+//! Funcion de callback para prender los segmentos de la pantalla
+typedef void (*display_segments_on_t)(uint8_t segments);
+
+//! Funcion de callback para prender un digito de la pantlla
+typedef void (*display_digit_on_t)(uint8_t digit);
+
+//! Estructura con las funciones de bajo nivel para manejo de la pantalla
+typedef struct display_driver_s {
+    display_screen_off_t ScreenTurnOff;   //!< Funcion para apagar los segmentos y digitos
+    display_segments_on_t SegmentsTurnOn; //!< Funcion para prender determinados segmentos
+    display_digit_on_t DigitTurnOn;       //!< Funcion para prender un digito
+} const * const display_driver_t;         //!< Puntero al controlador de pantalla
+
 /* === Public variable declarations ============================================================ */
+
+/**
+ * @brief Metodo para crear una pantalla multiplexada de siete segmentos
+ *
+ * @param digits Cantidad de digitos que forman la pantalla
+ * @param driver Puntero a la estructura con las funciones de bajo nivel
+ * @return display_t Puntero al descriptor de la pantalla creada
+ */
+display_t DisplayCreate(uint8_t digits, display_driver_t driver);
+
+/**
+ * @brief Funcion para escribir un numero BCD en la pantalla
+ *
+ * @param display Puntero al descriptor de la pantalla en la que se escribe
+ * @param number Puntero al primer elemento de numero BCD
+ * @param size Cantidad de elementos en el vector que contienen el numero BCD
+ */
+void DisplayWriteBCD(display_t display, uint8_t * number, uint8_t size);
+
+/**
+ * @brief Funcion para refrescar la pantalla
+ *
+ * @param display Puntero al descriptor de la pantalla que se debe refrescar
+ */
+void DisplayRefresh(display_t display);
 
 /* === Public function declarations ============================================================ */
 
--- src/display.c
+++ src/display.c
@@ -30,14 +30,40 @@ SPDX-License-Identifier: MIT
 /* === Headers files inclusions =============================================================== */
 
 #include "display.h"
+#include <string.h>
 
 /* === Macros definitions ====================================================================== */
+#ifndef DISPLAY_MAX_DIGITS
+#define DISPLAY_MAX_DIGITS 8
+#endif
 
 /* === Private data type declarations ========================================================== */
 
+struct display_s {
+    uint8_t digits;
+    uint8_t active_digit;
+    uint8_t memory[DISPLAY_MAX_DIGITS];
+    struct display_driver_s driver[1];
+};
+
 /* === Private variable declarations =========================================================== */
 
+static const uint8_t IMAGES[] = {
+    SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F,             //!< 0
+    SEGMENT_B | SEGMENT_C,                                                             //!< 1
+    SEGMENT_A | SEGMENT_B | SEGMENT_D | SEGMENT_E | SEGMENT_G,                         //!< 2
+    SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_G,                         //!< 3
+    SEGMENT_B | SEGMENT_C | SEGMENT_F | SEGMENT_G,                                     //!< 4
+    SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_F | SEGMENT_G,                         //!< 5
+    SEGMENT_A | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G,             //!< 6
+    SEGMENT_A | SEGMENT_B | SEGMENT_C,                                                 //!< 7
+    SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_D | SEGMENT_E | SEGMENT_F | SEGMENT_G, //!< 8
+    SEGMENT_A | SEGMENT_B | SEGMENT_C | SEGMENT_F | SEGMENT_G,                         //!< 9
+};
 /* === Private function declarations =========================================================== */
+
+// Funcion para asignar un descriptor para crear una nueva pantalla de siete segmentos
+static display_t DisplayAllocate(void);
 
 /* === Public variable definitions ============================================================= */
 
@@ -45,8 +71,41 @@ SPDX-License-Identifier: MIT
 
 /* === Private function implementation ========================================================= */
 
+display_t DisplayAllocate(void) {
+    static struct display_s instances[1] = {0};
+    return &instances[0];
+}
+
 /* === Public function implementation ========================================================== */
 
+display_t DisplayCreate(uint8_t digits, display_driver_t driver) {
+    display_t display = DisplayAllocate;
+
+    if (display) {
+        display->digits = digits;
+        display->active_digit = digits - 1;
+        memcpy(display->driver, driver, sizeof(display->driver));
+        memset(display->memory, 0, sizeof(display->memory));
+        display->driver->ScreenTurnOff;
+    }
+    return display;
+}
+
+void DisplayWriteBCD(display_t display, uint8_t * number, uint8_t size) {
+    memset(display->memory, 0, sizeof(display->memory));
+    for (int index = 0; index < size; index++) {
+        if (index >= display->digits)
+            break;
+        display->memory[index] = IMAGES[number[index]];
+    }
+}
+
+void DisplayRefresh(display_t display) {
+    display->driver->ScreenTurnOff();
+    display->active_digit = (display->active_digit + 1) % display->digits;
+    display->driver->SegmentsTurnOn(display->memory[display->active_digit]);
+    display->driver->DigitTurnOn(display->active_digit);
+}
 /* === End of documentation ==================================================================== */
 
 /** @} End of module definition for doxygen */
